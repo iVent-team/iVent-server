fc3a712cdc7c6db614410ec0ed27cf9ec314bef4
“nulLeeKH” <i_am@nulleekh.com>
2023-11-26 01:52:55 +0900

    fix: error on permission validators

--- src/infra/middleware/handler/security.ts
+++ src/infra/middleware/handler/security.ts
@@ -17,25 +17,16 @@ const validateAccessToken = async (
             accessToken,
         );
         if (id instanceof Error) {
-            return res.status(401).custom.send({
-                status: 'fail',
-                message: '접근 권한이 없습니다',
-            });
+            return res.custom.send(401, {});
         }
 
         const user = await userRepository.findOneById(id);
         if (!user) {
-            return res.status(401).custom.send({
-                status: 'fail',
-                message: '접근 권한이 없습니다',
-            });
+            return res.custom.send(401, {});
         }
         req.custom.user = user;
     } catch (e) {
-        return res.status(500).custom.send({
-            status: 'fail',
-            message: '알 수 없는 오류가 발생하였습니다',
-        });
+        return res.custom.send(500, {});
     }
     next();
 };
@@ -47,16 +38,10 @@ const validateIndividual = async (
 ) => {
     try {
         if (!req.custom.user.isIndividual) {
-            return res.status(401).custom.send({
-                status: 'fail',
-                message: '접근 권한이 없습니다',
-            });
+            return res.custom.send(401, {});
         }
     } catch (e) {
-        return res.status(500).custom.send({
-            status: 'fail',
-            message: '알 수 없는 오류가 발생하였습니다',
-        });
+        return res.custom.send(500, {});
     }
     next();
 };
@@ -64,16 +49,10 @@ const validateIndividual = async (
 const validateManager = async (req: Request, res: Response, next: Function) => {
     try {
         if (!req.custom.user.isManager) {
-            return res.status(401).custom.send({
-                status: 'fail',
-                message: '접근 권한이 없습니다',
-            });
+            return res.custom.send(401, {});
         }
     } catch (e) {
-        return res.status(500).custom.send({
-            status: 'fail',
-            message: '알 수 없는 오류가 발생하였습니다',
-        });
+        return res.custom.send(500, {});
     }
     next();
 };
