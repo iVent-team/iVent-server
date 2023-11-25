fb4f96ee1e25ebfa0d6755aa6a9affd039072e80
“nulLeeKH” <i_am@nulleekh.com>
2023-11-26 04:07:44 +0900

    fix: add ivent router in service

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import heartbeatRouter from './heartbeat';
+import iventRouter from './ivent';
 import manageRouter from './manage';
 import userRouter from './user';
 
 const router = express.Router();
 
 router.use('/heartbeat', heartbeatRouter);
+router.use('/ivent', iventRouter);
 router.use('/manage', manageRouter);
 router.use('/user', userRouter);
 
